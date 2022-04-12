3b7a3e1e835987085b8d08f9a3b525c6eee9226a
shashwat shukla <72383260+shashwatshukla123@users.noreply.github.com>
2022-04-12 16:55:00 +0530

    Add files via upload

---
+++ Class-161/shoot.js
@@ -0,0 +1,46 @@
+AFRAME.registerComponent("bullets", {
+    init: function () {
+      this.shootBullet();
+    },
+    shootBullet: function () {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "z") {
+          var bullet = document.createElement("a-entity");
+  
+          bullet.setAttribute("geometry", {
+            primitive: "sphere",
+            radius: 0.1,
+          });
+  
+          bullet.setAttribute("material", "color", "black");
+  
+          var cam = document.querySelector("#camera");
+  
+          pos = cam.getAttribute("position");
+  
+          bullet.setAttribute("position", {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+          });
+  
+          var camera = document.querySelector("#camera").object3D;
+          // To acess entity as the three.js object we can use the above line.
+  
+          //get the camera direction as Three.js Vector
+          var direction = new THREE.Vector3();
+          //camera.getWorldDirection(direction), will set the value of the vector given by the method in the direction variable.
+          camera.getWorldDirection(direction);
+          console.log(direction)
+  
+          //set the velocity and it's direction
+          bullet.setAttribute("velocity", direction.multiplyScalar(-10));
+  
+          var scene = document.querySelector("#scene");
+  
+          scene.appendChild(bullet);
+        }
+      });
+    },
+  });
+  
